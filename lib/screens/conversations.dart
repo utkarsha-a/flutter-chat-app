e909471241cf1024c798ffbc381cff322eee48b9
utkarsha-a <utkarsha.aherrao@gmail.com>
2025-12-15 12:16:17 +0530

    added messenger home screen UI

---
+++ lib/screens/conversations.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Conversations extends StatefulWidget {
+  const Conversations({super.key});
+
+  @override
+  State<Conversations> createState() => _ConversationsState();
+}
+
+class _ConversationsState extends State<Conversations> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Conversations Screen'),
+      ),
+    );
+  }
+}
